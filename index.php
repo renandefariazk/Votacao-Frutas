300a69b0f43004497a921225c0faadb66b457a34
Renan de Faria <renandefaria2009@hotmail.com>
2021-02-09 12:17:36 -0300

    CSS Inicial

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
-    $nome = $_GET['nome'];
-    $select = $_GET['select'];
+    // if(!empty($_GET['nome']) && !empty($_GET['select'])){
+    //     $nome = $_GET['nome'];
+    //     $select = $_GET['select'];
+    // }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,37 +15,44 @@
 </head>
 <body>
     <header>
-
+        <div class="divNav">
+            <nav><img src="./assets/GitHub-Mark github icone.png" alt="GitHub" class="gitHubNav"></nav>
+            <nav><img src="" alt="Linkedin"></nav>
+        </div>
     </header>
     <main>
-        <div class="imagensDiv">
-            <img src="" alt="Manga">
-            <img src="" alt="Uva">
-            <img src="" alt="Laranja">
+        <div class="afterHeader">
+            <div class="imagensDiv">
+                <img src="./assets/mangaFrutaFoto.jpg " alt="Manga" class="fotoFruta">
+                <img src="./assets/uvaFrutaFoto.jpg" alt="Uva" class="fotoFruta">
+                <img src="./assets/laranjaFrutaFoto.jpg " alt="Laranja" class="fotoFruta">
+            </div>
         </div>
-        <div>
-            <form action="index.php" method="get">
-                <input type="name" name="nome" placeholder="Nome">
-                <select name="select" id="escolha"></select>
+        <div class="divForm">
+            <form class="form" method="get">
+                <input type="name" name="nome" placeholder="Seu Nome" class="inputInf">
+                <select name="select" id="escolha" class="inputInf">
                     <option value="manga">manga</option>
                     <option value="uva">uva</option>
                     <option value="laranja">laranja</option>
+                    </select>
+                <input type="submit" class="inputInf">
             </form>
+            <div class="tableDiv">
+                <table class="table">
+                    <tr>
+                        <th>Fruta</th>
+                        <th>Votos</th>
+                    </tr>
+                    <?php
+                        // foreach(getVotos() as $votos){
+                        //     echo "<td>$votos[fruta]</td>
+                        //         <td>$votos[votos]</td>"
+                        // }
+                    ?>
+                </table>
+            </div>
         </div>
-        <!-- <div>
-            <table>
-                <tr>
-                    <th>Fruta</th>
-                    <th>Votos</th>
-                </tr>
-                <?php
-                    // foreach(getVotos() as $votos){
-                    //     echo "<td>$votos[fruta]</td>
-                    //         <td>$votos[votos]</td>"
-                    // }
-                ?>
-            </table>
-        </div> -->
     </main>
     <script type="module" src="./script.js"></script>
 </body>
